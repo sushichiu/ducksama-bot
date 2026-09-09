86e8550e4ceecf0e9788747cd4f43abe9fb88ebd
sushichiu <sushi.chiu@gmail.com>
2026-09-08 19:45:36 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -69,7 +69,7 @@ def main():
     app = Application.builder().token(BOT_TOKEN).post_init(post_init).build()
     app.job_queue.run_daily(
         send_daily_image,
-        time=time(hour=6, minute=0, tzinfo=TIMEZONE),
+       time=time(hour=7, minute=16, tzinfo=TIMEZONE),
         name="daily_image"
     )
     app.run_polling()
